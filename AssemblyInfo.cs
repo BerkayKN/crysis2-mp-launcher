10d50b6cba552ac069b2fc37accc4a18c447c323
BerkayKN <149893627+BerkayKN@users.noreply.github.com>
2024-09-25 21:36:09 +0300

    Added AssemblyInfo.cs for 1.0.0.3

---
+++ AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Windows;
+using System.Windows.Resources;
+
+[assembly: CompilationRelaxations(8)]
+[assembly: AssemblyTitle("Crysis 2 Multiplayer Launcher")]
+[assembly: AssemblyDescription("Crysis 2 Multiplayer Mod Launcher")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("C2COMMUNITY")]
+[assembly: AssemblyProduct("Crysis 2 Multiplayer Launcher")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyAssociatedContentFile("webview2loader.dll")]
+[assembly: AssemblyVersion("1.0.0.3")]
